a9f83c1ca5eca7ac6c486d45df32fb95565613c1
bulgariamitko <bulgaria_mitko@yahoo.com>
2018-02-02 11:13:07 +0200

    added some css to look more like the wireframes and fixed some bugs(js related)

--- demo_application/web/js/app.js
+++ demo_application/web/js/app.js
@@ -1,1 +1,13 @@
-$(document).foundation()
+$( document ).ready(function() {
+    $('input[name=answer]').click(function() {
+	    $(this).attr('selected', true);
+	    $('input[name=submitanswer]').click();
+	  });
+    $('input[name=setting]').click(function() {
+    	// remove style from current input
+    	$('.bg-success').removeClass('bg-success');
+    	$('.text-white').removeClass('text-white');
+    	// add style to the new input
+    	$(this).parent().addClass('text-white').parent().addClass('bg-success');
+    });
+});
